f3492e1a60d8a663d01e04b25e95c9be075eaed5
sosshik <caxalexal@gmail.com>
2023-11-06 19:10:07 +0200

    second commit

--- main.go
+++ main.go
@@ -4,22 +4,22 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"syscall"
 	"time"
 )
 
 func main() {
 	fmt.Println("Hello world")
 	start := time.Now()
-	timer := time.NewTimer(10 * time.Second)
-	sig := make(chan os.Signal, 1)
-	signal.Notify(sig, os.Interrupt)
-	go func() {
-		<-sig
-		end := time.Now()
-		timer.Stop()
-		fmt.Printf("Stopped by the user after %v seconds\n", end.Sub(start).Seconds())
-		os.Exit(1)
-	}()
-	<-timer.C
-	fmt.Println("Goodbye world")
+	timer := time.After(10 * time.Second)
+	interruptionChannel := make(chan os.Signal, 1)
+	signal.Notify(interruptionChannel, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+	select {
+	case <-interruptionChannel:
+		fmt.Printf("Stopped by the user after %v seconds\n", time.Since(start).Seconds())
+		return
+	case <-timer:
+		fmt.Println("Goodbye world")
+		return
+	}
 }
